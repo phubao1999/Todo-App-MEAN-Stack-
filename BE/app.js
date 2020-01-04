78e1f6384664dfc88142eac6450d1e2cd852830d
Bao Tuong <baophu@rionlab.com>
2020-01-04 19:43:39 +0700

    Update Server node

--- BE/app.js
+++ BE/app.js
@@ -1,1 +1,11 @@
-console.log('Test');
+const express = require('express');
+const app = express();
+const mongo = require('mongoose');
+
+app.get('/', (req, res) => {
+    res.send('We Are In Home');
+});
+
+app.listen(3000, () => {
+    console.log('Server Is Listening On http://localhost:3000/');
+});

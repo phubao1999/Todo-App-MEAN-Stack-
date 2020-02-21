45f82d4d3b3b26af924bfb56522250f300cf30ca
BaoPT <baophu@rionlab.com>
2020-02-21 15:14:44 +0700

    Set Router

--- BE/routes/index.js
+++ BE/routes/index.js
@@ -1,10 +1,6 @@
 const list = require('./list');
 const authentication = require('./users');
 
-// router.get('/', (req, res) => {
-//     res.send('Welcome To Home Page');
-// });
-
 class RouterIndex {
     constructor(app) {
         this.app = app;
